d10c10e78cafc00091202609e1883ecbdbab831e
Etienne LOUCHEZ <elouchez@e1r4p22.clusters.42paris.fr>
2022-12-21 17:50:30 +0100

    oskour

--- COMMAND/USER.cpp
+++ COMMAND/USER.cpp
@@ -7,18 +7,23 @@ void    user_cmd(socketRun server, std::string params, int id) {
     while (std::getline(ss, s, ' '))
         parts.push_back(s);
 
-    std::string message;
-    message = ":OurIRC ";
     if (parts.size() < 4) {
-        message += "461";
-        message += " * :Not enough parameters\r\n";
-        send(id, message.c_str(), message.length(), MSG_DONTWAIT);
+        std::cout << "LEL" << std::endl;
+        send_message(server, id, ERR_NEEDMOREPARAMS, "");
         return ;
     }
+    std::string name;
+    for (std::vector<std::string>::iterator it = parts[4]; it++; it = parts.end()) {
+        name += (*)it;
+    }
     std::string username = parts[1];
-    std::string realname = parts[4];
+    std::string realname = name;
 
+    std::cout << "KEK" << std::endl;
     server.getUserMap()[id]->setUsername(username);
     server.getUserMap()[id]->setRealname(realname);
-
+    send_message(server, id, RPL_WELCOME, "");
+    send_message(server, id, RPL_YOURHOST, "");
+    send_message(server, id, RPL_CREATED, "");
+    send_message(server, id, RPL_MYINFO, "");
 }
--- utils.cpp
+++ utils.cpp
@@ -23,7 +23,9 @@ void    send_message(socketRun server, int id_cli, int code, std::string str) {
         realCode = std::string(2, '0').append(codeStr);
     else if (code < 100)
         realCode = std::string(1, '0').append(codeStr);
-
+    else
+        realCode = codeStr;
+    
     std::string message = ":" + server.getHostname() + " " + realCode;
     if (server.getUserMap()[id_cli]->getNick().empty())
         message += " * ";
@@ -33,31 +35,31 @@ void    send_message(socketRun server, int id_cli, int code, std::string str) {
     switch (code) {
         // REPLIES
         case RPL_WELCOME:
-            message += "Welcome to our IRC server " + server.getUserMap()[id_cli]->getNick();
+            message += "Welcome to our IRC server " + server.getUserMap()[id_cli]->getNick(); break;
         case RPL_YOURHOST:
-            message += "Your host is " + server.getHostname() + ", running version Alpha 0.1";
+            message += "Your host is " + server.getHostname() + ", running version Alpha 0.1"; break;
         case RPL_CREATED:
-            message += "This server was created \"coder temps ecoule\"";
+            message += "This server was created \"coder temps ecoule\""; break;
         case RPL_MYINFO:
-            message += server.getHostname() + " Alpha 0.1 " + " Trucs a rajouter";
+            message += server.getHostname() + " Alpha 0.1 " + " Trucs a rajouter"; break;
 
         // ERRORS
         case ERR_NONICKNAMEGIVEN:
-            message += ":No nickname given";
+            message += ":No nickname given"; break;
         case ERR_ERRONEUSNICKNAME:
-            message += str + " :Erroneous nickname";
+            message += str + " :Erroneous nickname"; break;
         case ERR_NICKNAMEINUSE:
-            message += str + " :Nickname is already in use";
+            message += str + " :Nickname is already in use"; break;
         case ERR_NOTREGISTERED:
-            message += ":You have not registered";
+            message += ":You have not registered"; break;
         case ERR_NEEDMOREPARAMS:
-            message += str + " :Not enough parameters";
+            message += str + " :Not enough parameters"; break;
         case ERR_ALREADYREGISTRED:
-            message += ":Unauthorized command (already registered)";
+            message += ":Unauthorized command (already registered)"; break;
         case ERR_PASSWDMISMATCH:
-            message += ":Password incorrect";
+            message += ":Password incorrect"; break;
         case ERR_RESTRICTED:
-            message += ":Your connection is restricted!";
+            message += ":Your connection is restricted!"; break;
     }
 
     message += "\r\n";

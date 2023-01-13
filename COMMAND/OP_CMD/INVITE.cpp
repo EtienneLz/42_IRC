ec2711c8aeaf9ca80550b3080dcb62ed4acff073
chdespon <chdespon@student.42.fr>
2023-01-13 16:25:34 +0100

    INVITE CMD UPDATE

--- COMMAND/OP_CMD/INVITE.cpp
+++ COMMAND/OP_CMD/INVITE.cpp
@@ -27,21 +27,8 @@ void INVITE(Server *server, std::string params, int id)
 		return (send_message(server, id, ERR_CHANOPRIVSNEEDED, channel));
 	}
 
-	chan->joinChan(user);
-	std::vector<User *> chanUsers = chan->getUsers();
-	std::string message = ":" + target + "!" + user->getUsername()  + "@" + user->getHost() + " JOIN :" + channel + "\r\n";
-	for (std::vector<User *>::iterator it = chan->getUsers().begin(); it != chan->getUsers().end(); it++) {
-		std::cout << "REPLY CHAN --- " << message << std::endl;
-		send((*it)->getId(), message.c_str(), message.length(), MSG_DONTWAIT);
-	}
-
-	if (chan->getTopic() != "")
-		send_chan_message(server, user->getId(), RPL_TOPIC, chan->getTopic(), channel);
-	else
-		send_chan_message(server, user->getId(), RPL_NOTOPIC, "", channel);
-
 	std::string reply = ":" + server->getUserMap()[id]->getNick() + " INVITE " + target + " " + channel + "\r\n";
 	send(user->getId(), reply.c_str(), reply.size(), MSG_DONTWAIT);
-	NAMES(server, channel, user->getId());
-	send_chan_message(server, user->getId(), RPL_ENDOFNAMES, "", channel);
+
+	JOIN(server, channel, user->getId());
 }

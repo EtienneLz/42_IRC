60becd371e492adf401e7c9b78801fd9dc3ac17b
Marie SELIGNAC <mseligna@e1r8p15.clusters.42paris.fr>
2023-01-13 16:31:46 +0100

    mainCLIENT

---
+++ client_test.cpp
@@ -0,0 +1,45 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <string>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+//#define PORT 12345
+  
+int main(int argc, char const* argv[])
+{
+    (void)argc;
+    int PORT = atoi(argv[1]);
+    int sock = 0, valread, client_fd;
+    struct sockaddr_in serv_addr;
+    std::string hello = "Hello from client 1\n";
+    char buffer[1024] = { 0 };
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+  
+    // Convert IPv4 and IPv6 addresses from text to binary
+    // form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+    if ((client_fd = connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock, hello.c_str(), hello.length(), 0);
+    printf("Hello message sent 1\nsock = %d\n", sock);
+    valread = read(sock, buffer, 1024);
+    printf("%s\n", buffer);
+  
+    // closing the connected socket
+    close(client_fd);
+    return 0;
+}
